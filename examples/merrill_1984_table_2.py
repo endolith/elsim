2527059f26f17d8f0effca239a7f977ae6685f31
endolith <endolith@gmail.com>
2019-09-18 00:25:12 -0400

    Modify Table 1 to match Merrill 1984 Table 2
    Format actual table more nicely (should copy this change to
    other examples)

--- examples/merrill_1984_table_2.py
+++ examples/merrill_1984_table_2.py
@@ -1,8 +1,9 @@
 """
-Reproduce Table 1 and Figure 1
+Reproduce Table 2
 
-Condorcet Efficiencies for a Random Society
-(25 voters)
+Condorcet Efficiencies under Random Society and
+Spatial Model Assumptions
+(201 voters, 5 candidates)
 
 from
 
@@ -11,30 +12,34 @@ Electoral Systems", American Journal of Political Science, vol. 28,
 no. 1, pp. 23-48, 1984.  :doi:`10.2307/2110786`
 
 Typical result:
+Disp:       1.0	  1.0	  1.0	  1.0	  0.5	  0.5	  0.5	  0.5
+Corr:       0.5	  0.5	  0.0	  0.0	  0.5	  0.5	  0.0	  0.0
+D:          2.0	  4.0	  2.0	  4.0	  2.0	  4.0	  2.0	  4.0
+---------------------------------------------------------------------
+Plurality  57.5	 65.8	 62.2	 78.4	 21.7	 24.4	 27.2	 41.3
+Runoff     80.1	 87.3	 81.6	 93.6	 35.4	 42.2	 41.5	 61.5
+Hare       79.2	 86.7	 84.0	 95.4	 35.9	 46.8	 41.0	 69.9
+Approval   73.8	 77.8	 76.9	 85.4	 71.5	 76.4	 73.8	 82.7
+Borda      87.1	 89.3	 88.2	 92.3	 83.7	 86.3	 85.2	 89.4
+Coombs     97.8	 97.3	 97.9	 98.2	 93.5	 92.3	 93.8	 94.5
+Black     100.0	100.0	100.0	100.0	100.0	100.0	100.0	100.0
+SU max     82.9	 85.8	 85.3	 90.8	 78.1	 81.5	 80.8	 87.1
+CW         99.7	 99.7	 99.7	 99.6	 98.9	 98.6	 98.7	 98.5
 
-          2      3       4       5       7       10
-Plurality 100.0   79.1    68.4    61.8    51.4    41.1
-Runoff    100.0   96.2    89.6    83.4    72.3    60.3
-Hare      100.0   96.2    92.5    89.1    83.7    77.0
-Approval  100.0   75.6    70.0    67.4    63.8    61.2
-Borda     100.0   90.9    87.4    85.9    84.6    83.9
-Coombs    100.0   96.9    93.4    91.0    86.4    81.7
-Black     100.0  100.0   100.0   100.0   100.0   100.0
-SU max    100.0   84.1    79.6    78.4    77.3    77.5
-CW        100.0   91.7    83.1    75.6    64.3    52.9
+Many of these values match the paper closely, but some are consistently off by
+up to 4%.
 """
 import time
 from collections import Counter
 import numpy as np
-import matplotlib.pyplot as plt
 from elsim.methods import (fptp, runoff, irv, approval, borda, coombs,
                            black, utility_winner, condorcet)
-from elsim.elections import random_utilities
+from elsim.elections import normal_electorate, normed_dist_utilities
 from elsim.strategies import honest_rankings, approval_optimal
 
 n = 10_000
-n_voters = 25
-n_cands_list = (2, 3, 4, 5, 7, 10)
+n_voters = 201
+n_cands = 5
 
 ranked_methods = {'Plurality': fptp, 'Runoff': runoff, 'Hare': irv,
                   'Borda': borda, 'Coombs': coombs, 'Black': black}
@@ -43,85 +48,72 @@ rated_methods = {'SU max': utility_winner,
                  'Approval': lambda utilities, tiebreaker:
                      approval(approval_optimal(utilities), tiebreaker)}
 
-count = {key: Counter() for key in (ranked_methods.keys() |
-                                    rated_methods.keys() | {'CW'})}
-
 start_time = time.monotonic()
 
-for iteration in range(n):
-    for n_cands in n_cands_list:
-        utilities = random_utilities(n_voters, n_cands)
+#             disp, corr, D
+conditions = ((1.0, 0.5, 2),
+              (1.0, 0.5, 4),
+              (1.0, 0.0, 2),
+              (1.0, 0.0, 4),
+              (0.5, 0.5, 2),
+              (0.5, 0.5, 4),
+              (0.5, 0.0, 2),
+              (0.5, 0.0, 4),
+              )
+
+results = []
+
+for disp, corr, D in conditions:
+    print(disp, corr, D)
+
+    count = Counter()
+
+    for iteration in range(n):
+        v, c = normal_electorate(n_voters, n_cands, dims=D, corr=corr,
+                                 disp=disp)
 
         """
         "Simulated utilities were normalized by range, that is, each voter's
         set of utilities were linearly expanded so that the highest and lowest
         utilities for each voter were 1 and 0, respectively."
 
-        This is necessary for the SU Maximizer results to match Merrill's.
-        """
-        utilities -= utilities.min(1)[:, np.newaxis]
-        utilities /= utilities.max(1)[:, np.newaxis]
+        TODO: standard scores vs normalized don't matter for the ranked systems
+        and don't affect approval much
 
+        but This is necessary for the SU Maximizer results to match Merrill's.
+        """
+        utilities = normed_dist_utilities(v, c)
         rankings = honest_rankings(utilities)
 
         # If there is a Condorcet winner, analyze election, otherwise skip it
         CW = condorcet(rankings)
         if CW is not None:
-            count['CW'][n_cands] += 1
+            count['CW'] += 1
 
             for name, func in ranked_methods.items():
                 if func(rankings, tiebreaker='random') == CW:
-                    count[name][n_cands] += 1
+                    count[name] += 1
 
             for name, func in rated_methods.items():
                 if func(utilities, tiebreaker='random') == CW:
-                    count[name][n_cands] += 1
+                    count[name] += 1
+
+    results.append(count)
 
 elapsed_time = time.monotonic() - start_time
 print('Elapsed:', time.strftime("%H:%M:%S", time.gmtime(elapsed_time)), '\n')
 
-# Plot Merrill's results as dots for comparison
-merrill_table_1 = {
-    'Plurality': {2: 100.0, 3:  79.1, 4:  69.4, 5:  62.1, 7:  52.0, 10:  42.6},
-    'Runoff':    {2: 100.0, 3:  96.2, 4:  90.1, 5:  83.6, 7:  73.5, 10:  61.3},
-    'Hare':      {2: 100.0, 3:  96.2, 4:  92.7, 5:  89.1, 7:  84.8, 10:  77.9},
-    'Approval':  {2: 100.0, 3:  76.0, 4:  69.8, 5:  67.1, 7:  63.7, 10:  61.3},
-    'Borda':     {2: 100.0, 3:  90.8, 4:  87.3, 5:  86.2, 7:  85.3, 10:  84.3},
-    'Coombs':    {2: 100.0, 3:  96.3, 4:  93.4, 5:  90.2, 7:  86.1, 10:  81.1},
-    'Black':     {2: 100.0, 3: 100.0, 4: 100.0, 5: 100.0, 7: 100.0, 10: 100.0},
-    'SU max':    {2: 100.0, 3:  84.4, 4:  80.2, 5:  77.9, 7:  77.2, 10:  77.8},
-    'CW':        {2: 100.0, 3:  91.6, 4:  83.4, 5:  75.8, 7:  64.3, 10:  52.5},
-    }
-
-plt.figure(f'Figure 1. {n_voters} voters, {n} iterations')
-for method in ('Plurality', 'Runoff', 'Hare', 'Approval', 'Borda', 'Coombs',
-               'Black'):
-    x, y = zip(*sorted(merrill_table_1[method].items()))
-    plt.plot(x, y, '.')
-
-# Restart color cycle, so result colors match
-plt.gca().set_prop_cycle(None)
-
-# Number of candidates
-print('', *n_cands_list, sep='\t')
+print('Disp:    ', '\t'.join(f'{v: >5.1f}' for v in np.array(conditions).T[0]))
+print('Corr:    ', '\t'.join(f'{v: >5.1f}' for v in np.array(conditions).T[1]))
+print('D:       ', '\t'.join(f'{v: >5.1f}' for v in np.array(conditions).T[2]))
+print('-'*69)
 
 # Of those elections with CW, likelihood that method chooses CW
-x_cw, y_cw = zip(*sorted(count['CW'].items()))
+y_cw = np.array([c['CW'] for c in results])
 for method in ('Plurality', 'Runoff', 'Hare', 'Approval', 'Borda', 'Coombs',
-               'Black'):
-    x, y = zip(*sorted(count[method].items()))
-    plt.plot(x, np.array(y)/y_cw*100, '-', label=method)
-    print(method + '\t', '\t'.join(f'{v:.1f}' for v in np.array(y)/y_cw*100))
-
-# Likelihood that social utility maximizer is Condorcet Winner
-x, y = zip(*sorted(count['SU max'].items()))
-print('SU max\t', '\t'.join(f'{v:.1f}' for v in np.array(y)/y_cw*100))
+               'Black', 'SU max'):
+    y = np.array([c[method] for c in results])
+    print(f'{method: <9}', '\t'.join(f'{v: >5.1f}' for v in y/y_cw*100))
 
 # Likelihood of Condorcet Winner (normalized by n iterations)
-print('CW\t', '\t'.join(f'{v:.1f}' for v in np.asarray(y_cw)/n*100))
-
-plt.legend()
-plt.grid(True, color='0.7', linestyle='-', which='major', axis='both')
-plt.grid(True, color='0.9', linestyle='-', which='minor', axis='both')
-plt.ylim(40, 102)
-plt.xlim(1.8, 10.2)
+print('CW       ', '\t'.join(f'{v: >5.1f}' for v in y_cw/n*100))
