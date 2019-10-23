Fix SUE calculation bug, improve accuracy
It was calculating UW's utility twice ('UW" and "SU Max"), and not
calculating SUE correctly.  This makes the results closer to Merrill's,
but still not exact.

examples/merrill_1984_table_4.py
@@ -11,21 +11,26 @@ S. Merrill III, "A Comparison of Efficiency of Multicandidate
 Electoral Systems", American Journal of Political Science, vol. 28,
 no. 1, pp. 23-48, 1984.  :doi:`10.2307/2110786`
 
-Typical result:
-Disp:       1.0   1.0     1.0     1.0     0.5     0.5     0.5     0.5
-Corr:       0.5   0.5     0.0     0.0     0.5     0.5     0.0     0.0
-D:          2.0   4.0     2.0     4.0     2.0     4.0     2.0     4.0
----------------------------------------------------------------------
-Plurality  75.9  81.2    81.5    92.7    14.9    18.9    30.2    54.4
-Runoff     91.7  94.7    92.2    97.6    43.6    51.3    56.1    75.9
-Hare       92.8  95.2    94.6    98.4    52.5    62.9    61.2    84.1
-Approval   96.7  97.4    97.0    98.6    96.2    97.2    96.0    98.0
-Borda      98.1  98.7    98.5    99.5    97.1    98.0    97.5    99.0
-Coombs     97.3  97.6    97.9    98.9    94.6    94.8    95.5    96.7
-Black      97.6  97.9    98.2    99.1    95.9    96.5    96.7    98.0
+Typical result with 100_000 simulations:
+
+    Disp:       1.0   1.0     1.0     1.0     0.5     0.5     0.5     0.5
+    Corr:       0.5   0.5     0.0     0.0     0.5     0.5     0.0     0.0
+    D:          2.0   4.0     2.0     4.0     2.0     4.0     2.0     4.0
+    ---------------------------------------------------------------------
+    Plurality  72.1  79.1    80.4    92.4     4.0     6.3    25.2    52.9
+    Runoff     90.5  94.2    92.0    97.5    36.6    43.6    53.3    75.3
+    Hare       91.7  94.7    94.3    98.4    46.4    57.7    58.7    83.6
+    Approval   96.2  97.0    96.8    98.5    95.6    96.8    95.8    98.0
+    Borda      97.8  98.6    98.3    99.4    96.6    97.7    97.4    99.0
+    Coombs     97.0  97.5    97.7    98.7    94.0    94.3    95.0    96.7
+    Black      97.3  97.8    98.0    99.0    95.5    96.1    96.5    98.0
+
+Many of these values match the paper closely, but some are consistently off by
+up to 9%.
 """
 import time
 from collections import Counter
+from random import randint
 import numpy as np
 from elsim.methods import (fptp, runoff, irv, approval, borda, coombs,
                            black, utility_winner)
@@ -80,9 +85,9 @@ for disp, corr, D in conditions:
         utilities = normed_dist_utilities(v, c)
         rankings = honest_rankings(utilities)
 
-        # Find the social utility winner and accumulate utilities
-        UW = utility_winner(utilities)
-        count['UW'] += utilities.sum(0)[UW]
+        # Pick a random winner and accumulate utilities
+        RW = randint(0, n_cands - 1)
+        count['RW'] += utilities.sum(0)[RW]
 
         for name, func in rated_methods.items():
             winner = func(utilities, tiebreaker='random')
@@ -104,9 +109,10 @@ print('-'*69)
 
 # Calculate Social Utility Efficiency from summed utilities
 y_uw = np.array([c['SU max'] for c in results])
+y_rw = np.array([c['RW'] for c in results])
 
 for method in ('Plurality', 'Runoff', 'Hare', 'Approval', 'Borda', 'Coombs',
                'Black'):
     y = np.array([c[method] for c in results])
-    SUE = (np.array(y) - n_voters * n / 2)/(np.array(y_uw) - n_voters * n / 2)
+    SUE = (y - y_rw)/(y_uw - y_rw)
     print(f'{method: <9}', '\t'.join(f'{v: >5.1f}' for v in SUE*100))