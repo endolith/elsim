8070f3f529b47e8a90850fc86f133813b85b0583
endolith <endolith@gmail.com>
2021-02-10 22:56:32 -0500

    Add Vote-for-k, requirements for np.argpartition

--- elsim/strategies/__init__.py
+++ elsim/strategies/__init__.py
@@ -4,4 +4,4 @@ Implements various strategies.
 These take collections of utilities as inputs and return collections of
 ballots that voters cast for a voting method.
 """
-from .strategies import honest_rankings, approval_optimal
+from .strategies import honest_rankings, approval_optimal, vote_for_k
--- elsim/strategies/strategies.py
+++ elsim/strategies/strategies.py
@@ -117,3 +117,80 @@ def approval_optimal(utilities):
     means = np.mean(utilities, 1)
     approvals = (utilities > means[:, np.newaxis]).astype(np.uint8)
     return approvals
+
+
+def vote_for_k(utilities, k):
+    """
+    Convert utilities to approval voting ballots, approving top k candidates.
+
+    Given a set of utilities for each voter-candidate pair, each voter is
+    modeled as voting for the top `k` candidates.[1]_
+
+    Parameters
+    ----------
+    utilities : array_like
+        A 2D collection of utilities.
+
+        Rows represent voters, and columns represent candidate IDs.
+        Higher utility numbers mean greater approval of that candidate by that
+        voter.
+    k : int or 'half'
+        The number of candidates approved of by each voter, or 'half' to make
+        the number dependent on the number of candidates.
+
+    Returns
+    -------
+    election : ndarray
+        A 2D collection of approval ballots.
+
+        Rows represent voters, and columns represent candidate IDs.
+        A cell contains 1 if that voter approves of that candidate,
+        otherwise 0.
+
+    References
+    ----------
+    .. [1] Weber, Robert J. (1978). "Comparison of Public Choice Systems".
+       Cowles Foundation Discussion Papers. Cowles Foundation for Research in
+       Economics. No. 498. https://cowles.yale.edu/publications/cfdp/cfdp-498
+
+    Examples
+    --------
+    Voter 0 loves Candidates A (index 0) and B (index 1), but hates C (2).
+    Voter 1 dislikes A, likes B, and loves C.
+    Voter 2 hates A, and is lukewarm about B and C.
+
+    >>> utilities = [[1.0, 1.0, 0.0],
+                     [0.1, 0.8, 1.0],
+                     [0.0, 0.5, 0.5],
+                     ]
+
+    Each voter approves of their top-two candidates:
+    Voter 0 approves A and B.
+    Voter 1 approves B and C.
+    Voter 2 approves B and C.
+
+    >>> vote_for_k(utilities, 2)
+    array([[1, 1, 0],
+           [0, 1, 1],
+           [0, 1, 1]], dtype=uint8)
+
+    """
+    utilities = np.asarray(utilities)
+    n_cands = utilities.shape[1]
+    if k == 'half':
+        # "It is interesting to observe that the vote-for-k and vote-for-(m-k)
+        # voting systems are equally effective."
+        # So for 7 candidates, we could use either k=4 or k=3 (= 7//2)
+        k = n_cands // 2
+    elif k <= 0 or k > n_cands:
+        raise ValueError(f'k of {k} not possible with {n_cands} candidates')
+
+    # Efficiently get indices of top k candidates for each voter
+    # https://stackoverflow.com/a/23734295/125507
+    # TODO: How are tied utilities handled, such as top 2 with 3 tied? Random?
+    top_k = np.argpartition(utilities, -k, axis=1)[:, -k:]
+    approvals = np.zeros(utilities.shape, np.uint8)
+
+    # TODO: Not sure if this is the most efficient way
+    approvals[np.arange(len(approvals))[:, np.newaxis], top_k] = 1
+    return approvals
