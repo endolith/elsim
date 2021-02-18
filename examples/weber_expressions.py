677e640c805924b7dd66aa2ebc5c371c28504685
endolith <endolith@gmail.com>
2021-02-17 23:44:06 -0500

    Add Weber expressions to test against

---
+++ examples/weber_expressions.py
@@ -0,0 +1,158 @@
+"""
+Weber's effectiveness expressions, for comparison.
+
+Weber, Robert J. (1978). "Comparison of Public Choice Systems".
+Cowles Foundation Discussion Papers. Cowles Foundation for Research in
+Economics. No. 498. https://cowles.yale.edu/publications/cfdp/cfdp-498
+
+These all use the impartial culture / random society model and an infinite
+number of voters.
+"""
+from math import sqrt
+from numpy.testing import assert_, assert_almost_equal
+
+
+def eff_standard(m):
+    """
+    Calculate effectiveness of Standard FPTP voting system.
+
+    Parameters
+    ----------
+    m : int
+        Total number of candidates.
+
+    Returns
+    -------
+    eff : float
+        Effectiveness of FPTP with `m` candidates and infinite number of
+        random voters.
+    """
+    return sqrt(3*m)/(m+1)
+
+
+def eff_vote_for_k(m, k):
+    """
+    Calculate effectiveness of the "vote for `k`" approval voting variant.
+
+    Parameters
+    ----------
+    m : int
+        Total number of candidates.
+    k : int
+        Number of candidates that each voter approves of.
+
+    Returns
+    -------
+    eff : float
+        Effectiveness of "vote-for-`k`", with `m` candidates and infinite
+        number of random voters.
+    """
+    return 1/(m+1) * sqrt(3*m*k*(m-k) / (m-1))
+
+
+def eff_vote_for_half(m):
+    """
+    Calculate effectiveness of the "vote-for-half" approval voting variant.
+
+    Parameters
+    ----------
+    m : int
+        Total number of candidates.
+
+    Returns
+    -------
+    eff : float
+        Effectiveness of "vote-for-half", with `m` candidates and infinite
+        number of random voters.
+    """
+    if not m % 2:  # m is even
+        return sqrt(3)/2 * m/(m+1) * sqrt(m/(m-1))
+    else:  # m is odd
+        return sqrt(3)/2 *           sqrt(m/(m+1))
+
+
+# TODO: Or implement this by using the vote for k function
+# Or use that as a test
+
+
+def eff_vote_for_or_against_k(m, k):
+    """
+    Calculate effectiveness of the "vote-for-or-against-k" voting system.
+
+    This is a variant of combined approval voting (CAV) in which every voter
+    approves or disapproves of `k` candidates.
+
+    Parameters
+    ----------
+    m : int
+        Total number of candidates.
+    k : int
+        Number of candidates that each voter approves or disapproves of.
+
+    Returns
+    -------
+    eff : float
+        Effectiveness of "vote-for-or-against-`k`", with `m` candidates and
+        infinite number of random voters.
+    """
+    if m % 2:  # m is odd
+        return (5*m**2 + 1 - 6*m*k) / (4*(m+1)) * sqrt(3*k/(m * (m-1) * (m-k)))
+    else:  # m is even
+        return (5*m**2     - 6*m*k) / (4*(m+1)) * sqrt(3*k/(m * (m-1) * (m-k)))
+
+
+def eff_borda(m):
+    """
+    Calculate effectiveness of the Borda count voting system.
+
+    Parameters
+    ----------
+    m : int
+        Total number of candidates.
+
+    Returns
+    -------
+    eff : float
+        Effectiveness of Borda count with `m` candidates and infinite number
+        of random voters.
+    """
+    return sqrt(m / (m+1))
+
+
+def test_cases():
+    # Verify various statements from the paper
+    """
+    When `m` is even and `k = m/2`, the vote-for-or-against-k system coincides
+    with the vote-for-k system.
+    """
+    for m in range(2, 100, 2):
+        k = m // 2
+        assert_almost_equal(eff_vote_for_or_against_k(m, k),
+                            eff_vote_for_k(m, k))
+
+    """
+    For k < m/2, it is easily verified that the vote-for-or-against-k voting
+    system is strictly more effective than the vote-for-k system.
+    """
+    for m in range(2, 15):
+        for k in range(1, (m+1)//2):
+            assert_(eff_vote_for_or_against_k(m, k) > eff_vote_for_k(m, k))
+
+    """
+    For three-candidate elections we see that the standard voting system
+    (75% effective) is appreciably less effective than the approval voting
+    system (87.5%)
+    """
+    assert_almost_equal(eff_standard(3), 0.75)
+    assert_almost_equal(eff_vote_for_or_against_k(3, 1), 0.875)
+
+    """
+    A few values from the table
+    """
+    assert_almost_equal(eff_standard(10), 49.79/100, decimal=4)
+    assert_almost_equal(eff_vote_for_half(5), 79.06/100, decimal=4)
+    assert_almost_equal(eff_best_vote_for_or_against_k(4), 80.83/100, 4)
+    assert_almost_equal(eff_borda(6), 92.58/100, decimal=4)
+
+
+test_cases()
