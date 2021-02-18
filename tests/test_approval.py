1efa8dfa0ad9227b086dcf2934875f4a933097bf
endolith <endolith@gmail.com>
2021-02-18 00:38:37 -0500

    Add combined_approval tests by parametrizing

--- tests/test_approval.py
+++ tests/test_approval.py
@@ -4,7 +4,7 @@ import pytest
 from hypothesis import given
 from hypothesis.strategies import integers, tuples
 from hypothesis.extra.numpy import arrays
-from elsim.methods import approval
+from elsim.methods import approval, combined_approval
 
 
 def collect_random_results(method, election):
@@ -21,8 +21,10 @@ def collect_random_results(method, election):
     return winners
 
 
+# combined_approval should pass every test approval does, but not vice versa
+@pytest.mark.parametrize("method", [approval, combined_approval])
 @pytest.mark.parametrize("tiebreaker", [None, 'random', 'order'])
-def test_basic(tiebreaker):
+def test_basic(tiebreaker, method):
     # Standard Tennessee example
     # https://electowiki.org/wiki/Approval_voting#Example
     #                 Memphis Nashville Chattanooga Knoxville
@@ -32,7 +34,7 @@ def test_basic(tiebreaker):
                 *17*[[0,      0,        1,          1]],
                 ]
 
-    assert approval(election, tiebreaker) == 1  # Nashville
+    assert method(election, tiebreaker) == 1  # Nashville
 
     # Example from http://www-users.math.umn.edu/~rogness/math1001/approval/
     #                 Gore McCain Bush
@@ -42,7 +44,7 @@ def test_basic(tiebreaker):
                 *20*[[0,   0,     1]],
                 ]
 
-    assert approval(election, tiebreaker) == 1  # McCain
+    assert method(election, tiebreaker) == 1  # McCain
 
     # Example from
     # https://bluecc.instructure.com/courses/4190/files/244440/
@@ -55,10 +57,11 @@ def test_basic(tiebreaker):
                 *30*[[1, 0, 1, 0]],
                 ]
 
-    assert approval(election, tiebreaker) == 0  # A
+    assert method(election, tiebreaker) == 0  # A
 
 
-def test_ties():
+@pytest.mark.parametrize("method", [approval, combined_approval])
+def test_ties(method):
     # Two-way tie between candidates 1 and 2
     election = np.array([[0, 1, 1],
                          [1, 0, 1],
@@ -67,13 +70,13 @@ def test_ties():
                          [0, 1, 1],
                          ])
     # No tiebreaker:
-    assert approval(election, tiebreaker=None) is None
+    assert method(election, tiebreaker=None) is None
 
     # Mode 'order' should always prefer lowest candidate ID
-    assert approval(election, tiebreaker='order') == 1
+    assert method(election, tiebreaker='order') == 1
 
     # Mode 'random' should choose all tied candidates at random
-    assert collect_random_results(approval, election) == {1, 2}
+    assert collect_random_results(method, election) == {1, 2}
 
     # Three-way tie between 0, 1, and 2
     election = np.array([[0, 1, 1],
@@ -88,13 +91,13 @@ def test_ties():
                          ])
 
     # No tiebreaker:
-    assert approval(election, tiebreaker=None) is None
+    assert method(election, tiebreaker=None) is None
 
     # Mode 'order' should always prefer lowest candidate ID
-    assert approval(election, tiebreaker='order') == 0
+    assert method(election, tiebreaker='order') == 0
 
     # Mode 'random' should choose all tied candidates at random
-    assert collect_random_results(approval, election) == {0, 1, 2}
+    assert collect_random_results(method, election) == {0, 1, 2}
 
 
 def approval_ballots(min_cands=1, max_cands=25, min_voters=1, max_voters=100):
@@ -106,23 +109,25 @@ def approval_ballots(min_cands=1, max_cands=25, min_voters=1, max_voters=100):
     return arrays(np.uint, tuples(n_voters, n_cands), elements=integers(0, 1))
 
 
+@pytest.mark.parametrize("method", [approval, combined_approval])
 @pytest.mark.parametrize("tiebreaker", ['random', 'order'])
 @given(election=approval_ballots(min_cands=1, max_cands=25,
                                  min_voters=1, max_voters=100))
-def test_legit_winner(election, tiebreaker):
+def test_legit_winner(election, tiebreaker, method):
     election = np.asarray(election)
     n_cands = election.shape[1]
-    winner = approval(election, tiebreaker)
+    winner = method(election, tiebreaker)
     assert isinstance(winner, int)
     assert winner in range(n_cands)
 
 
+@pytest.mark.parametrize("method", [approval, combined_approval])
 @given(election=approval_ballots(min_cands=1, max_cands=25,
                                  min_voters=1, max_voters=100))
-def test_legit_winner_none(election):
+def test_legit_winner_none(election, method):
     election = np.asarray(election)
     n_cands = election.shape[1]
-    winner = approval(election)
+    winner = method(election)
     assert isinstance(winner, (int, type(None)))
     assert winner in set(range(n_cands)) | {None}
 
