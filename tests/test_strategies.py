9f5323fe09b2a5bfbfa95dd508fa2d9bb0f3aeb7
endolith <endolith@gmail.com>
2021-02-23 00:59:56 -0500

    Add hypothesis tests for strategies

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -2,6 +2,9 @@ import random
 import numpy as np
 from numpy.testing import assert_array_equal
 import pytest
+from hypothesis import given
+from hypothesis.strategies import integers, tuples, floats
+from hypothesis.extra.numpy import arrays
 from elsim.strategies import approval_optimal, vote_for_k
 
 
@@ -71,6 +74,32 @@ def test_invalid_k(k):
         election = [[0, 1],
                     [1, 0]]
         vote_for_k(election, k)
+
+
+def utilities(min_cands=2, max_cands=25, min_voters=1, max_voters=100):
+    """
+    Strategy to generate utilities arrays
+    """
+    n_cands = integers(min_value=min_cands, max_value=max_cands)
+    n_voters = integers(min_value=min_voters, max_value=max_voters)
+    return arrays(np.float, tuples(n_voters, n_cands), elements=floats(0, 1))
+
+
+@given(utilities=utilities())
+def test_approval_optimal_properties(utilities):
+    utilities = np.asarray(utilities)
+    election = approval_optimal(utilities)
+    assert election.shape == utilities.shape
+    assert set(election.flat) <= {0, 1}
+
+
+@given(utilities=utilities())
+def test_vote_for_k_properties(utilities):
+    utilities = np.asarray(utilities)
+    election = vote_for_k(utilities, 1)
+    assert election.shape == utilities.shape
+    assert 1 in set(election.flat)
+    assert set(election.flat) <= {0, 1}
 
 
 if __name__ == "__main__":
