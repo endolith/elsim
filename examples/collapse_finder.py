cdd2a1b50b8d0e256dc0a1facddf1f9dac2b7ef5
endolith <endolith@gmail.com>
2023-07-26 02:27:46 -0400

    save progress

--- examples/collapse_finder.py
+++ examples/collapse_finder.py
@@ -13,8 +13,8 @@ from elsim.elections import normal_electorate, normed_dist_utilities
 from elsim.methods import fptp, irv
 from elsim.strategies import honest_rankings
 
-n_voters = 1_000
-n_cands = 3
+n_voters = 1_0 # 00
+n_cands = 9
 cand_dist = 'normal'
 
 
@@ -25,51 +25,90 @@ def human_format(num):
         num /= 1000.0
 
 
+# ChatGPT
+def top_n_indices(arr, n):
+    return arr.argsort()[-n:][::-1]
+
+
+# ChatGPT
+def closest_to_zero_indices(arr, n):
+    return np.abs(arr).argsort(axis=0)[:n]
+
+
 for trial in range(1000):
     v, c = normal_electorate(n_voters, n_cands, dims=1)
     utilities = normed_dist_utilities(v, c)
     rankings = honest_rankings(utilities)
 
-    winner = fptp(rankings, tiebreaker='random')
-    winner = irv(rankings, tiebreaker='random')
+    # First remove the least 4 in FPTP ballot
+    election = np.asarray(rankings)
 
+    # Get first preferences from election array
+    first_preferences = election[:, 0]
 
-print(f'{n_batches} tasks total:')
+    # Tally all first preferences (with index of tally = candidate ID)
+    tallies = np.bincount(first_preferences)
 
-# Create a list of jobs
-jobs = []
+    # Find the set of 5 candidates who have the highest tally
+    n = 5
+    winner_indices = top_n_indices(tallies, n)
 
-# Add jobs to the list
-for _ in range(n_elections):
-    jobs.append(delayed(simulate_batch)(n_voters, n_cands, batch_size))
+    # Find the remaining losers
+    loser_indices = set(range(n_cands)) - set(winner_indices)
 
-# Execute the jobs in parallel
-results = Parallel(n_jobs=-3, verbose=5)(jobs)
+    # Find the best candidates
+    best_indices = closest_to_zero_indices(c, n)
 
-winners = {k: [v for d in results for v in d[k]] for k in results[0]}
+    if set(winner_indices) == set(best_indices):
+        print('found after', trial)
 
-title = f'{method}, {human_format(n_elections)} 1D elections, '
-title += f'{human_format(n_voters)} voters, '
-title += f'{human_format(n_cands)} '
-title += cand_dist + 'ly-distributed candidates'
+#     est_tally = max(tallies)
+#     winners = _all_indices(tallies, highest_tally)
 
-# For plotting only
-v, c = normal_electorate(n_voters, 1000, dims=1)
+#     # Break any ties using specified method
+#     tiebreak = _get_tiebreak(tiebreaker, _tiebreak_map)
+#     # return tiebreak(winners)[0]
 
-fig, ax = plt.subplots(nrows=len(winners), num=title, sharex=True,
-                       constrained_layout=True, figsize=(7.5, 9.5))
-fig.suptitle(title)
-for n, disp in enumerate(winners):
-    histplot(winners[disp], ax=ax[n], label=f'{disp:.1f} dispersion',
-             stat='density')
-    ax[n].set_yticklabels([])  # Don't care about numbers
-    ax[n].set_ylabel("")  # No "density"
+#     winner = fptp(rankings, tiebreaker='random')
+#     winner = irv(rankings, tiebreaker='random')
 
-    tmp = ax[n].twinx()
-    kdeplot(v[:, 0], ax=tmp, ls=':', label='Voters')  # Label doesn't work
-    ax[n].plot([], [], ls=':', label='Voters')  # Dummy label hack
-    tmp.set_yticklabels([])  # Don't care about numbers
-    tmp.set_ylabel("")  # No "density"
+# raise SystemExit
+# # print(f'{n_batches} tasks total:')
 
-    ax[n].legend()
-ax[0].set_xlim(-2.5, 2.5)
+# # Create a list of jobs
+# jobs = []
+
+# # Add jobs to the list
+# for _ in range(n_elections):
+#     jobs.append(delayed(simulate_batch)(n_voters, n_cands, batch_size))
+
+# # Execute the jobs in parallel
+# results = Parallel(n_jobs=-3, verbose=5)(jobs)
+
+# winners = {k: [v for d in results for v in d[k]] for k in results[0]}
+
+# title = f'{method}, {human_format(n_elections)} 1D elections, '
+# title += f'{human_format(n_voters)} voters, '
+# title += f'{human_format(n_cands)} '
+# title += cand_dist + 'ly-distributed candidates'
+
+# # For plotting only
+# v, c = normal_electorate(n_voters, 1000, dims=1)
+
+# fig, ax = plt.subplots(nrows=len(winners), num=title, sharex=True,
+#                        constrained_layout=True, figsize=(7.5, 9.5))
+# fig.suptitle(title)
+# for n, disp in enumerate(winners):
+#     histplot(winners[disp], ax=ax[n], label=f'{disp:.1f} dispersion',
+#              stat='density')
+#     ax[n].set_yticklabels([])  # Don't care about numbers
+#     ax[n].set_ylabel("")  # No "density"
+
+#     tmp = ax[n].twinx()
+#     kdeplot(v[:, 0], ax=tmp, ls=':', label='Voters')  # Label doesn't work
+#     ax[n].plot([], [], ls=':', label='Voters')  # Dummy label hack
+#     tmp.set_yticklabels([])  # Don't care about numbers
+#     tmp.set_ylabel("")  # No "density"
+
+#     ax[n].legend()
+# ax[0].set_xlim(-2.5, 2.5)
